0e7fe4ced8c3615454bed751c508f50c851a480f
MichalWolnyDev <mwolny98@interia.pl>
2018-09-06 14:09:17 +0200

    Ball Game
    add goals, players collision with walls, change background

--- BALL_1V1_PROJECT/script.js
+++ BALL_1V1_PROJECT/script.js
@@ -1,24 +1,31 @@
 var player_one;
 var player_two;
 var ball;
+var goal_one;
+var goal_two;
+var po_points = 0, pt_points = 0;
 
 function play(){
   pitch.start();
   pitch.elements();
-  player_one = new Player(30, 30, 250, 300, "white");
-  player_two = new Player(30, 30, 750, 300, "blue");
+  player_one = new Player(30, 30, 250, 350, "white");
+  player_two = new Player(30, 30, 750, 350, "blue");
   ball = new Ball(500, 300, "aqua");
+  goal_one = new Goal(10, 100, 0, 300, "white");
+  goal_two = new Goal(10, 100, 1035, 300, "blue");
+
 
 }
 // Boisko
 var pitch = {
   canvas: document.createElement('canvas'),
   start: function(){
-    this.canvas.width = 1000;
-    this.canvas.height = 600;
+    this.canvas.width = 1045;
+    this.canvas.height = 700;
     this.canvas.id = 'canvas';
     this.canvas.className = 'canvas';
     this.ctx = this.canvas.getContext('2d');
+
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updatePitch, 20); //gra dziala w 50 klatkach/s
 
@@ -95,45 +102,56 @@ var Player = function(width, height, x, y, color){
 
 
 }
-// Sterowanie
-// document.onkeydown = checkKey;
-//
-// function checkKey(e){
-//   e = e || window.event;
-//
-//   if(e.keyCode == '37'){
-//     player_one.speedX -= 10;
-//   }
-//   else if(e.keyCode == '38'){
-//     player_one.speedY -= 10;
-//   }
-//   else if(e.keyCode == '39'){
-//     player_one.speedX += 10;
-//   }
-//   else if(e.keyCode == '40'){
-//     player_one.speedY += 10;
-//   }
-//
-// }
+//konstruktor bramki
+var Goal = function(width, height, x, y, color){
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function(){
+    ctx = pitch.ctx;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
 // Czyszczenie i rysowanie boiska na nowo
 function updatePitch(){
   pitch.clear();
+
   player_one.speedX = 0;
   player_one.speedY = 0;
 
-  if(pitch.keys && pitch.keys[37]) {
+  if(pitch.keys && pitch.keys[65]) {
     player_one.speedX -= 6;
   }
-  if(pitch.keys && pitch.keys[38]) {
+  if(pitch.keys && pitch.keys[87]) {
     player_one.speedY -= 6;
   }
-  if(pitch.keys && pitch.keys[39]) {
+  if(pitch.keys && pitch.keys[68]) {
     player_one.speedX = 6;
   }
-  if(pitch.keys && pitch.keys[40]) {
+  if(pitch.keys && pitch.keys[83]) {
     player_one.speedY = 6;
   }
 
+  player_two.speedX = 0;
+  player_two.speedY = 0;
+
+  if(pitch.keys && pitch.keys[37]) {
+    player_two.speedX -= 6;
+  }
+  if(pitch.keys && pitch.keys[38]) {
+    player_two.speedY -= 6;
+  }
+  if(pitch.keys && pitch.keys[39]) {
+    player_two.speedX = 6;
+  }
+  if(pitch.keys && pitch.keys[40]) {
+    player_two.speedY = 6;
+  }
+
+  // odbicie pilki
   if(ball.x + ball.dx > canvas.width - ball.r || ball.x + ball.dx < ball.r){
      ball.dx = -ball.dx;
   }
@@ -143,11 +161,27 @@ function updatePitch(){
 
   ball.x += ball.dx;
   ball.y += ball.dy;
+  // odbicie pilki od gracza
+
+
+
+  //kolizja ze sciana player_one
+  if(player_one.x < 0) player_one.x = 0;
+  if(player_one.y < 0) player_one.y = 0;
+  if(player_one.x + player_one.width > canvas.width) player_one.x = canvas.width - player_one.width;
+  if(player_one.y + player_one.height > canvas.height) player_one.y = canvas.height - player_one.height;
+  //kolizja ze sciana player_two
+  if(player_two.x < 0) player_two.x = 0;
+  if(player_two.y < 0) player_two.y = 0;
+  if(player_two.x + player_two.width > canvas.width) player_two.x = canvas.width - player_two.width;
+  if(player_two.y + player_two.height > canvas.height) player_two.y = canvas.height - player_two.height;
 
   ball.position();
   ball.update();
   player_one.position();
   player_one.update();
+  player_two.position();
   player_two.update();
-
+  goal_one.update();
+  goal_two.update();
 }
